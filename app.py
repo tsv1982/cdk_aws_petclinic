cfff0c0e1923abbfd01192700a3c935c1c8eaf8c
tsv1982 <tsv198250@gmail.com>
2022-11-15 20:01:29 +0200

    002

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 import aws_cdk as cdk
 
 from infra.lib.CdkPipeline import CdkPipeline
+#from infra.lib.push_image import push_image
 
 app = cdk.App()
 cdk_pipeline = CdkPipeline(app, "CdkPipelinePetclinic")
+#push_image = push_image(app, "PushImage")
+
 
 app.synth()
